d6b9ed3ce432dcb9e86ece2da5c850799e4e31f8
Taiwo Daniel <jborland705@gmail.com>
2024-10-10 15:32:18 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+//DOM Manipulation
+
+//accessing elements by id
+
+
+//accessing elements by class name
+
+
+
+//accessing elements by tagname
+
+
+//accessing elements by query selector
+
+//accessing elements by query selector all
+
+
+//changing CSS Styling on the DOM
+
+DataTransferItemList.style.padding ='10px';
+//Creating Elements
+
+//appending Elements
+const container =document.querySelector(new_title)
+li.textcontent = Games  thrones
+//Modifying Elements
+
+//modifying attributes using setAttribute and remove attribute and classes using classlist
+
+//adding class elements
